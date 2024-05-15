4cdc0e043fadab41e5208171624cfeee848d86e4
HESRON BECKER <hesron_becker@estudante.sesisenai.org.br>
2024-05-15 20:42:27 -0300

    Atualização DELETE

--- src/routes/router.js
+++ src/routes/router.js
@@ -10,16 +10,17 @@ router.post('/livros/:id/delete', (req, res) => {
   const id = parseInt(req.params.id);
   const produtoExcluido = ProdutosDAO.deletar(id);
   if (produtoExcluido) {
-      res.render('exclusaoConfirmada', { produto: produtoExcluido });
+    res.redirect('/exclusaoConfirmada');
   } else {
-      res.status(404).send('Produto não encontrado');
+    res.status(404).send('Produto não encontrado');
   }
 });
 
-// Página de confirmação de exclusão
 router.get('/exclusaoConfirmada', (req, res) => {
   res.render('exclusaoConfirmada');
 });
+
+module.exports = router;
 
 // Importa e utiliza as rotas de produtos
 const produtosRoutes = require('./produtos');
