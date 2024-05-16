0f82eaa38e86d2164366035021e78682ac57616f
HESRON BECKER <hesron_becker@estudante.sesisenai.org.br>
2024-05-15 21:56:42 -0300

    Atualização UPDATE

--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -8,6 +8,11 @@ router.get('/', produtosController.list);
 // Rota para mostrar um produto (READ)
 router.get('/:id', produtosController.show);
 
+router.post('/', produtosController.create);
+
+// Rota para excluir um produto (DELETE)
+router.put('/:id', produtosController.update);
+
 // Rota para excluir um produto (DELETE)
 router.delete('/:id', produtosController.delete);
 
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const router = new Router();
+const Produto = require("../models/produto")
 const ProdutosDAO = require('../models/dao/ProdutosDAO');
 
 router.get('/', (req, res) => {
   res.status(200).render("index")
 })
 
-router.post('/livros/:id/delete', (req, res) => {
+router.post('/produtos/:id/delete', (req, res) => {
   const id = parseInt(req.params.id);
   const produtoExcluido = ProdutosDAO.deletar(id);
   if (produtoExcluido) {
@@ -20,7 +21,37 @@ router.get('/exclusaoConfirmada', (req, res) => {
   res.render('exclusaoConfirmada');
 });
 
-module.exports = router;
+// Exibir formulário de edição
+router.get('/produtos/:id/edit', (req, res) => {
+  const id = parseInt(req.params.id);
+  const produto = ProdutosDAO.buscarPorId(id);
+  if (produto) {
+    res.render('produtosEdit', { produto: produto });
+  } else {
+    res.status(404).send('Produto não encontrado');
+  }
+});
+
+// Lidar com a submissão do formulário de edição
+router.post('/produtos/:id/edit', (req, res) => {
+  const id = parseInt(req.params.id);
+  const { nome, preco } = req.body;
+  const produtoAtualizado = new Produto({ id, nome, preco });
+  ProdutosDAO.atualizar(id, produtoAtualizado);
+  res.redirect('/produtos');
+});
+
+router.get('/produtos/create', (req, res) => {
+  res.render('produtosCreate');
+});
+
+// Lidar com a submissão do formulário de criação
+router.post('/produtos', (req, res) => {
+  const { nome, preco } = req.body;
+  const novoProduto = { nome, preco };
+  ProdutosDAO.criar(novoProduto);
+  res.redirect('/produtos');
+});
 
 // Importa e utiliza as rotas de produtos
 const produtosRoutes = require('./produtos');
