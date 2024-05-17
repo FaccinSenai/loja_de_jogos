259db27695dc0642449f1e87db34f06b16b0629a
HESRON BECKER <hesron_becker@estudante.sesisenai.org.br>
2024-05-16 21:10:17 -0300

    Atualização CREATE

--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -8,7 +8,14 @@ router.get('/', produtosController.list);
 // Rota para mostrar um produto (READ)
 router.get('/:id', produtosController.show);
 
-router.post('/', produtosController.create);
+// Rota para exibir o formulário de criação de um novo produto
+router.get('/create', produtosController.formCreate);
+
+// Rota para criar um novo produto (CREATE)
+router.post('/create', produtosController.create);
+
+// Rota para exibir o formulário de edição de um produto (READ)
+router.get('/:id/edit', produtosController.formEdit);
 
 // Rota para excluir um produto (DELETE)
 router.put('/:id', produtosController.update);
